af7dae707221f7d4d39920e472021662b8aeaa63
ijlal123 <ijlal.tanveer291@gmail.com>
2026-01-21 03:19:25 +0500

    added boiler code for input handling

---
+++ include/Input.h
@@ -0,0 +1,54 @@
+#pragma once
+
+class Input {
+    
+public:
+
+    static Input* Instance();
+
+public:
+
+    bool IsXClicked();
+    bool IsKeyPressed();
+
+    char GetKeyUp();
+    char GetKeyDown();
+
+    bool IsLeftButtonClicked();
+    bool IsRightButtonClicked();
+    bool IsMiddleButtonClicked();
+
+    int GetMousePositionX();
+    int GetMousePositionY();
+
+    int GetMouseMotionX();
+    int GetMouseMotionY();
+
+public:
+
+    void Update();
+
+private:
+
+    Input();
+    Input(const Input&);
+    Input& operator=(Input&);
+
+private:
+
+    char m_keyUp;
+    char m_keyDown;
+
+    bool m_isXClicked;
+    bool m_isKeyPressed;
+
+    bool m_isLeftButtonClicked;
+    bool m_isRightButtonClicked;
+    bool m_isMiddleButtonClicked;
+
+    int m_mouseMotionX;
+    int m_mouseMotionY;
+
+    int m_mousePositionX;
+    int m_mousePositionY;
+};
